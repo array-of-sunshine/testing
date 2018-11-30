3eba27fef5d19026aba62ffc4655dddc0815a06b
Brian <riceb53@gmail.com>
2018-11-29 21:25:39 -0600

    test add for calculator class

---
+++ calculator.rb
@@ -0,0 +1,53 @@
+require 'rspec'
+
+class Calculator
+  def add(number_one, number_two)
+    return number_one + number_two
+  end
+
+  def subtract(number_one, number_two)
+    return number_one - number_two
+  end
+
+  def multiply(number_one, number_two)
+    return number_one * number_two
+  end
+
+  def divide(dividend, divisor)
+    return dividend / divisor
+  end
+
+  def sqaure(number)
+    return square * square
+  end
+
+  def power(number, exponent)
+    return number ** exponent
+  end
+end
+
+# driver code
+calculator = Calculator.new
+
+# p "About to test the add method"
+# if calculator.add(3,5) == 8
+#   p "test passes"
+# else
+#   p "test fails"
+# end
+
+# rspec
+RSpec.describe Calculator do
+  describe '#add' do
+    it 'should return the sum of two numbers' do
+      calculator = Calculator.new
+      result = calculator.add(3,5)
+      expect(result).to eq(8)
+    end
+    it 'should return the sum of two negative numbers' do
+      calculator = Calculator.new
+      result = calculator.add(-3,-5)
+      expect(result).to eq(-8)
+    end
+  end
+end
