20201ee40ddeb8b79fedb8f181b0a98296a2f399
Brian <riceb53@gmail.com>
2018-12-02 11:56:29 -0600

    refactor to DRY up while loops

--- change_machine.rb
+++ change_machine.rb
@@ -2,22 +2,13 @@ require 'rspec'
 
 class ChangeMachine
   def change(cents)
+    coins = [25,10,5,1]
     money = []
-    while cents >= 25
-      money << 25
-      cents -= 25
-    end
-    while cents >= 10
-      money << 10
-      cents -= 10
-    end
-    while cents >= 5
-      money << 5
-      cents -= 5
-    end
-    while cents > 0
-      money << 1
-      cents -= 1
+    coins.each do |coin|
+      while cents >= coin
+        money << coin
+        cents -= coin
+      end
     end
     return money
   end
@@ -27,21 +18,25 @@ end
 RSpec.describe ChangeMachine do
   describe '#change' do
     it 'should return [1] when given 1' do
+      # skip
       machine = ChangeMachine.new
       result = machine.change(1)
       expect(result).to eq([1])
     end
     it 'should return [1,1] when given 2' do
+      # skip
       machine = ChangeMachine.new
       result = machine.change(2)
       expect(result).to eq([1, 1])
     end
     it 'should return [5,1] when given 6' do
+      # skip
       machine = ChangeMachine.new
       result = machine.change(6)
       expect(result).to eq([5, 1])
     end
     it 'should return [10,5,1,1,1,1] when given 19' do
+      # skip
       machine = ChangeMachine.new
       result = machine.change(19)
       expect(result).to eq([10,5,1,1,1,1])
