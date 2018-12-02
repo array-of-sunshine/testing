19252dc09ffe87574a684aa8d8811574599a3960
Brian <riceb53@gmail.com>
2018-12-02 09:16:33 -0600

    add comments about testing structure

--- calculator.rb
+++ calculator.rb
@@ -37,17 +37,20 @@ calculator = Calculator.new
 # end
 
 # rspec
-RSpec.describe Calculator do
-  describe '#add' do
+RSpec.describe Calculator do  # all the code that tests the calculator class
+  describe '#add' do # all the code for the add method
     it 'should return the sum of two numbers' do
       calculator = Calculator.new
-      result = calculator.add(3,5)
+      result = calculator.add(3, 5)
       expect(result).to eq(8)
     end
     it 'should return the sum of two negative numbers' do
       calculator = Calculator.new
-      result = calculator.add(-3,-5)
+      result = calculator.add(-3, -5)
       expect(result).to eq(-8)
     end
+  end
+  describe '#subtract' do
+    # all the code to test the subtract method
   end
 end
